db12bcd255667bcaf6b4c98de562094b3e20127b
up1046836 <up1046836@upnet.gr>
2023-05-18 03:07:00 +0300

    Update binary tree

--- data-structures/bt.py
+++ data-structures/bt.py
@@ -32,6 +32,10 @@ class BinaryTree:
 
     def __init__(self, iterable=None):
         self.root = BinaryTree.Node()
+        
+        if iterable:
+            for element in iterable:
+                self.insert(element)
 
     def search(self, value, node=None):
         if not node: node = self.root
@@ -42,8 +46,52 @@ class BinaryTree:
     def height(self, node=None):
         # maximum depth of external nodes
         if not node: node = self.root
-        if node.is_external(): return 0
+        if node.is_external(): return -1
         else: return 1 + max(self.height(node.left), self.height(node.right))
+
+    def inorder(self, node=None):
+        if not node: node = self.root
+        if node.is_internal():
+            self.inorder(node.left)
+            print(node)
+            self.inorder(node.right)
+
+    def remove_above_external(self, node):
+        if node.left.is_external():
+            if node == self.root: 
+                node.right.parent = None
+                self.root = node.right
+            elif node.parent.left == node: 
+                node.right.parent = node.parent
+                node.parent.left = node.right
+            else: 
+                node.right.parent = node.parent
+                node.parent.right = node.right
+        else: 
+            if node == self.root: 
+                node.left.parent = None
+                self.root = node.left
+            elif node.parent.left == node:
+                node.left.parent = node.parent
+                node.parent.left = node.left
+            else: 
+                node.left.parent = node.parent
+                node.parent.right = node.left
+
+    def remove(self, value):
+        current = self.search(value)
+        if current.is_external(): return None
+        elif current.left.is_external() or current.left.is_external():
+            self.remove_above_external(current)
+        else:
+            right_min = self.find_min(current.right)
+            current.value = right_min.value
+            self.remove_above_external(right_min)
+
+    def find_min(self, node=None):
+        if not node: node = self.root
+        if node.is_external(): return node.parent
+        else: return self.find_min(node.left)
 
     def insert(self, value):
         if not self.root:
